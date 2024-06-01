c7161240ca984305d1d64fbf280d4efe81e1cb39
David Wu <KyosukeNo1@gmail.com>
2024-06-01 01:07:29 -0700

    Enable AzureOpenAI

--- examples/retrieval/server.py
+++ examples/retrieval/server.py
@@ -2,12 +2,19 @@
 """Example LangChain server exposes a retriever."""
 from fastapi import FastAPI
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 
 from langserve import add_routes
 
+# Environment variables for Azure OpenAI
+# export OPENAI_API_TYPE=azure
+# export OPENAI_API_BASE=<openai-api-base>
+# export OPENAI_API_KEY=<openai-api-key>
+# export OPENAI_API_VERSION=2024-02-15-preview
+
 vectorstore = FAISS.from_texts(
-    ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+    # ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+    ["cats like fish", "dogs like sticks"], embedding=AzureOpenAIEmbeddings()
 )
 retriever = vectorstore.as_retriever()
 
