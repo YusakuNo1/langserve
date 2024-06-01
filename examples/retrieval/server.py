813b651cd3a0b087933d185aa807c4bf8c25efd9
David Wu <KyosukeNo1@gmail.com>
2024-06-01 12:58:15 -0700

    Simplify Azure initialization code

--- examples/retrieval/server.py
+++ examples/retrieval/server.py
@@ -6,15 +6,23 @@ from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 
 from langserve import add_routes
 
-# Environment variables for Azure OpenAI
-# export OPENAI_API_TYPE=azure
-# export OPENAI_API_BASE=<openai-api-base>
-# export OPENAI_API_KEY=<openai-api-key>
-# export OPENAI_API_VERSION=2024-02-15-preview
+# # OpenAI API key should only use env var: OPENAI_API_KEY
+# embedding = OpenAIEmbeddings()
+
+# Azure OpenAI API key should only use the following variables
+azure_openai_api_type = "azure"
+azure_openai_api_base = "https://" + <azure-ai-service-id> + ".openai.azure.com/openai/deployments/" + <azure-deployment> + "/"
+azure_openai_api_key = <azure-openai-api-key>
+azure_openai_api_version = "2024-02-15-preview"
+embedding = AzureOpenAIEmbeddings(
+    openai_api_type=azure_openai_api_type,
+    openai_api_base=azure_openai_api_base,
+    openai_api_key=azure_openai_api_key,
+    openai_api_version=azure_openai_api_version,
+)
 
 vectorstore = FAISS.from_texts(
-    # ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
-    ["cats like fish", "dogs like sticks"], embedding=AzureOpenAIEmbeddings()
+    ["cats like fish", "dogs like sticks"], embedding=embedding
 )
 retriever = vectorstore.as_retriever()
 
