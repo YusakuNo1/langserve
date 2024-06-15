9cb282b880cb0fd4f2947c225c1ef57137e3ec82
David Wu <KyosukeNo1@gmail.com>
2024-06-14 19:44:05 -0700

    Working good for AzureOpenAI: "agent" and "retrieval"

---
+++ examples/retrieval/server.david.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""Example LangChain server exposes a retriever."""
+from fastapi import FastAPI
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
+
+from langserve import add_routes
+
+# # OpenAI API key should only use env var: OPENAI_API_KEY
+# embedding = OpenAIEmbeddings()
+
+# Azure OpenAI API key should only use the following variables
+azure_openai_api_type = "azure"
+azure_openai_api_base = "https://daviwu-ai-2024-aoai395546270926.openai.azure.com/openai/deployments/text-embedding-ada-002/"
+azure_openai_api_key = "29e2045dc0fd4c8584cd2da656fe5327"
+azure_openai_api_version = "2024-02-15-preview"
+embedding = AzureOpenAIEmbeddings(
+    openai_api_type=azure_openai_api_type,
+    openai_api_base=azure_openai_api_base,
+    openai_api_key=azure_openai_api_key,
+    openai_api_version=azure_openai_api_version,
+)
+
+vectorstore = FAISS.from_texts(
+    ["cats like fish", "dogs like sticks"], embedding=embedding
+)
+retriever = vectorstore.as_retriever()
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="Spin up a simple api server using Langchain's Runnable interfaces",
+)
+# Adds routes to the app for using the retriever under:
+# /invoke
+# /batch
+# /stream
+add_routes(app, retriever)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
